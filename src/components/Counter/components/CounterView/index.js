aa459fac8cf3a0d864d2d6fd0b10a57839feb5e0
MikitaMastserau <nik.redko@gmail.com>
2023-03-01 13:50:22 +0300

    add counter

---
+++ src/components/Counter/components/CounterView/index.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+import styles from "./styles.module.scss";
+
+export const CounterView = ({counterValue, handleDecrement, handleReset, handleIncrement}) => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.display}>
+                {counterValue}
+            </div>
+            <div className={styles.controlButtons}>
+                <button className={styles.controlButton} onClick={handleDecrement}>-</button>
+                <button className={styles.controlButton} onClick={handleReset}>Reset</button>
+                <button className={styles.controlButton} onClick={handleIncrement}>+</button>
+            </div>
+        </div>
+    );
+};
+
+CounterView.propTypes = {
+    counterValue: PropTypes.number.isRequired,
+    handleDecrement: PropTypes.func.isRequired,
+    handleReset: PropTypes.func.isRequired,
+    handleIncrement: PropTypes.func.isRequired,
+};

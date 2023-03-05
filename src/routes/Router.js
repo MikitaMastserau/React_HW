8942ed08b0598c17ff18770886b03050dc5fe039
MikitaMastserau <nik.redko@gmail.com>
2023-03-05 18:09:43 +0300

    add headings for pages

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -5,10 +5,12 @@ import { ROUTE_NAMES } from "./routeNames";
 import { UsersContainer } from "../pages/Users/containers/UsersContainer";
 import { CounterContainer } from "../pages/Counter/containers/CounterContainer";
 
+import headingStyles from "../components/HeadingStyles/styles.module.scss"
+
 export const Router = () => {
     return (
         <Routes>
-            <Route path={ROUTE_NAMES.HOME} element={<h1>HOME PAGE</h1>} />
+            <Route path={ROUTE_NAMES.HOME} element={<h1 className={headingStyles.heading}>HOME PAGE</h1>} />
             <Route path={ROUTE_NAMES.USERS} element={<UsersContainer/>} />
             <Route path={ROUTE_NAMES.COUNTER} element={<CounterContainer/>} />
         </Routes>
